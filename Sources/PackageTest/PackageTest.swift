cbfbe7d418385179ef34260ecaf96e61e3f6dfa3
phora-sequenex <patrik.hora@sequenex.com>
2023-08-30 14:46:12 +0200

    Add XCFramework

---
+++ Sources/PackageTest/PackageTest.swift
@@ -0,0 +1,6 @@
+public struct PackageTest {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/PackageTestTests/PackageTestTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PackageTest
+
+final class PackageTestTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PackageTest().text, "Hello, World!")
+    }
+}
