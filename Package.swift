997b9fc9f04856c916739b8c7b2d243421417fa7
phora-sequenex <patrik.hora@sequenex.com>
2023-08-31 10:20:13 +0200

    New version 1.0.0

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "PackageTest",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
